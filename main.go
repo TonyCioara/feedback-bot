7136b049ca79238c0e784c8d1d14cfd645a1ed8d
Tony Cioara <tonyangelo9707@gmail.com>
2019-03-04 14:59:32 -0800

    Setting up heroku scheduler

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/TonyCioara/feedback-bot/slack"
@@ -12,6 +13,12 @@ import (
 // DO NOT EDIT THIS FUNCTION. This is a fully complete implementation.
 // export GO111MODULE=on; go run main.go
 func main() {
+	if len(os.Args) >= 2 && os.Args[1] == "my_job_param" {
+		// Execute job.
+		fmt.Println("Reload")
+		return
+	}
+	// Set up my web server with port, router, etc.
 	botToken := os.Getenv("BOT_OAUTH_ACCESS_TOKEN")
 	slackClient := slack.CreateSlackClient(botToken)
 	slack.RespondToEvents(slackClient)
